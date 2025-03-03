d18db5ec79d759a6735002e8db92e34237bf0566
Corbett Winningham <corbettbw@gmail.com>
2025-03-03 16:54:35 -0700

    adding pizza request tests

--- spec/requests/pizzas_spec.rb
+++ spec/requests/pizzas_spec.rb
@@ -1,11 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe "Pizzas", type: :request do
-  describe "GET /index" do
+  let!(:topping) { Topping.create(name: "Mushroom", emoji: "🍄") }
+  let!(:pizza) { Pizza.create(name: "Mushroom Pizza", topping_ids: [topping.id]) }
+
+  describe "GET /pizzas" do
     it "returns http success" do
-      get "/pizzas"
+      get pizzas_path
       expect(response).to have_http_status(:success)
     end
+
+    it "shows existing pizzas and their toppings" do
+      get pizzas_path
+      expect(response).to have_http_status(:success)
+      expect(response.body).to include("Mushroom Pizza")
+      expect(response.body).to include(topping.emoji)
+      expect(response.body).to include(topping.name)
+    end
   end
+
+  # describe "POST /pizzas" do
+
+  # end
+
+  # describe "DESTROY /pizzas" do
+
+  # end
 
 end
